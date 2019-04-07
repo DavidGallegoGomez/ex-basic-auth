23d291c4e08ffd65d96687371c763e50deb5af38
Carlos del Prado <carlos.delprado@11paths.com>
2019-04-07 18:55:57 +0200

    feat: delete user

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,5 +4,6 @@ const secure = require('../middlewares/secure.mid');
 const users = require('../controllers/users.controller');
 
 router.get('/', secure.isAuthenticated, users.list);
+router.post('/:id/delete', secure.isAuthenticated, users.delete);
 
 module.exports = router;
