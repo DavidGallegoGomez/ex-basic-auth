924919de5f5b10464ab9b873e5bda4d3491c9772
Carlos del Prado <carlos.delprado@11paths.com>
2019-04-07 17:46:53 +0200

    feat: register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,34 @@
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
 
 module.exports.register = (req, res, next) => {
   res.render('auth/register');
+}
+
+module.exports.doRegister = (req, res, next) => {
+
+  function renderWithErrors(errors) {
+    res.render('auth/register', {
+      user: req.body,
+      errors: errors
+    })
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        renderWithErrors({ email: 'Email already registered'})
+      } else {
+        user = new User(req.body);
+        return user.save()
+          .then(user => res.redirect('/login'))
+      }
+    })
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(error.errors)
+      } else {
+        next(error);
+      }
+    });
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const auth = require('../controllers/auth.controller');
 
 router.get('/register', auth.register);
+router.post('/register', auth.doRegister);
 
 module.exports = router;
